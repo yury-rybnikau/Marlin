296c1531d23593fa8ed2869d1148f021e177db8a
yrybnikov <yury.rybnikov@expcapital.com>
2020-04-26 18:54:55 +0300

    fix steps for titan aero

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -734,7 +734,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 480 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 457 }
 
 /**
  * Default Max Feed Rate (mm/s)
